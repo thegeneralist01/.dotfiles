9f6b8016916c5b924731b8f19cfb892029dedc20
TheGeneralist <180094941+thegeneralist01@users.noreply.github.com>
2025-04-04 15:18:24 +0200

    change nvim.cmp to blink

--- dotfiles/nvim/.config/nvim/lua/thegeneralist/lazy/treesitter.lua
+++ dotfiles/nvim/.config/nvim/lua/thegeneralist/lazy/treesitter.lua
@@ -3,6 +3,8 @@ return {
     build = ":TSUpdate",
     config = function()
       require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+        },
         -- A list of parser names, or "all" (the five listed parsers should always be installed)
         --[[
         ensure_installed = {
